1b8de1d4099ef062c8e75db3d5c4923ef531bb62
Mohammed A. Abo Sido <abosido2@gmail.com>
2023-01-18 13:25:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./main.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        Home
+      </div>
+    </>
+  );
+}
+
+export default App;
